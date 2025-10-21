5f7dc69252de29656bb8e97943d44eed721cf650
rootanvir <tanvir.22464751@gmail.com>
2025-10-21 14:54:36 +0600

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/global.css';
+import { Children } from 'react';
+export const metadata = {
+    title : "Promtopia",
+    description : 'Discover & Share API Prompts'
+}
+
+const Rootlayout = ({children}) =>{
+    return(
+        <html lang="en">
+            <body>
+                <div className='main'>
+                    <div className='gradient'/> 
+                </div>
+
+                <main className='app'>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+export default Rootlayout
