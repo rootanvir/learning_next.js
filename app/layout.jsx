dbb91a9bd0dbdfc1adc88b13ca4499a80381064a
rootanvir <tanvir.22464751@gmail.com>
2025-10-22 00:14:32 +0600

    css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import '@styles/global.css';
 import { Children } from 'react';
 export const metadata = {
-    title : "Promtopia",
+    title : "Rootlayout",
     description : 'Discover & Share API Prompts'
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,16 @@
 const Home = () =>{
     return(
-        <div>Page</div>
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text_center">
+                Discover & Share
+                <br className="max-md:hidden"/>
+                <span className="orange_gradient"> AI- powered Prompts</span>
+            </h1>
+            <p className="desc text-center">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus iste voluptates maxime,
+                dicta voluptatum recusandae labore odio rerum. Quas, odio? 
+            </p>
+        </section>
     )
 }
 export default Home
