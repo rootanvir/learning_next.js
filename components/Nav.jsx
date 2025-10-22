77e8b83123bc89df0a22284ea9cdda0635e75b61
rootanvir <tanvir.22464751@gmail.com>
2025-10-22 14:32:18 +0600

    button added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import '@styles/global.css';
+
+
 import React from 'react';
 import Link from 'next/link'
 import Image from 'next/image'
@@ -7,8 +10,9 @@ import { userState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLogin = true;
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
+    <nav className="flex justify-between items-center w-full mb-16 pt-3">
       <Link href="/" className='flex gap-2'>
         <Image
           src="/assets/images/logo.svg"
@@ -17,7 +21,21 @@ const Nav = () => {
           width={30}
           height={30}
         />
+        <p className='logo_text'>Rootanvir</p>
       </Link>
+      {/*Mobile Navigation */}
+      <div className='sm:flex hidden'>
+        {isUserLogin ?(
+          <div className='flex gap-3 md:gap-5'>
+            <Link href="/create-prompt" 
+            className='black_btn'>
+              Create Post
+            </Link>
+          </div>
+        ):(
+          <></>
+        )}
+      </div>
     </nav>
   );
 }
