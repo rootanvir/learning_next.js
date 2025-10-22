3258f51ab948ad287eaa939785a40daabe65145f
rootanvir <tanvir.22464751@gmail.com>
2025-10-22 10:16:16 +0600

    tailwind css fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,27 @@
-const Home = () =>{
-    return(
-        <section className="w-full flex-center flex-col">
+import Feed from '@components/Feed'
+
+const Home = () => {
+    return (
+        
+        <section className="w-full flex flex-col justify-center items-center text-center">
             <h1 className="head_text text_center">
                 Discover & Share
-                <br className="max-md:hidden"/>
-                <span className="orange_gradient"> AI- powered Prompts</span>
+                <br className="max-md:hidden" />
+                <span className="orange_gradient"> AI-powered Prompts</span>
             </h1>
-            <p className="desc text-center">
+
+            <p className="desc text-center mt-4">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus iste voluptates maxime,
-                dicta voluptatum recusandae labore odio rerum. Quas, odio? 
+                dicta voluptatum recusandae labore odio rerum. Quas, odio?
             </p>
+            <Feed />
+            <div className="text-blue-800">
+                Tailwind is working!
+            </div>
+
         </section>
+        
+
     )
 }
 export default Home
